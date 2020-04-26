b4084378786d95bb43a3cbe8b5131813c58a5253
jquinnie <jenn8866@gmail.com>
2020-04-26 16:48:29 -0600

    Add functionality to the commentList component

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
-};
+class CommentList extends Component {
+  // helper method that takes comment provided as a prop and generates an li element
+  renderComments() {
+    return this.props.comments.map((comment, idx) => {
+      return <li key={idx}>{comment}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
