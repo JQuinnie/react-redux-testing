4a9e99fc06d8911855adc749068f50fe70564e05
jquinnie <jenn8866@gmail.com>
2020-04-25 21:24:29 -0600

    Grouping together similar tests in describe

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -23,30 +23,28 @@ it('has a textarea and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  // 1. find textarea element
-  // 2. simulate a 'change' event (onChange is the name of a callback that we are passing to the textarea)
-  // 2nd arg is a mock object that tricks the event handler, handleChange
-  // 3. provide a fake event object, callback handler receives a piece of new state
-  wrapped.find('textarea').simulate('change', { target: { value: 'test comment' } });
-  // 4. force the component to update (rerender) because setState is asynchronous
-  wrapped.update();
-  // 5. assert that the textarea value has changed
-  expect(wrapped.find('textarea').prop('value')).toEqual('test comment');
-});
+describe('the text area', () => {
+  beforeEach(() => {
+    // 1. find textarea element
+    // 2. simulate a 'change' event (onChange is the name of a callback that we are passing to the textarea)
+    // 2nd arg is a mock object that tricks the event handler, handleChange
+    // 3. provide a fake event object, callback handler receives a piece of new state
+    wrapped.find('textarea').simulate('change', { target: { value: 'test comment' } }); // change event to set value
+    // 4. force the component to update (rerender) because setState is asynchronous
+    wrapped.update(); // make sure textarea gets new value
+  });
 
-// simulate a submit event on form not on button
-it('should empty  text area when form is submitted', () => {
-  // change event to set value
-  wrapped.find('textarea').simulate('change', { target: { value: 'test comment' } });
-  // make sure textarea gets new value
-  wrapped.update();
+  it('has a text area that users can type in', () => {
+    // 5. assert that the textarea value has changed
+    expect(wrapped.find('textarea').prop('value')).toEqual('test comment');
+  });
 
-  // expect(wrapped.find('textarea').prop('value')).toEqual('test comment'); // good form if it wasnt tested above
-
-  // simulate submit form
-  wrapped.find('form').simulate('submit');
-  // force setState to update since its async
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+  // simulate a submit event on form not on button
+  it('should empty text area when form is submitted', () => {
+    // simulate submit form
+    wrapped.find('form').simulate('submit');
+    // force setState to update since its async
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
